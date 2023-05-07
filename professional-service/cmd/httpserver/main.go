91d25ae6382d5fd616ce842e41647d0ebed8162b
hulkdx <hulktroll@gmail.com>
2023-05-07 17:39:31 +0300

    Gracefully shutdown the app

--- professional-service/cmd/httpserver/main.go
+++ professional-service/cmd/httpserver/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/config"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/router"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -16,6 +23,22 @@ func main() {
 		WriteTimeout: cfg.Server.WriteTimeout,
 		IdleTimeout:  cfg.Server.IdleTimeout,
 	}
-	server.ListenAndServe()
-	// TODO: shutdown gracefully
+
+	quit := make(chan os.Signal)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+		quit <- syscall.SIGTERM
+	}()
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
+	// TODO: close db connection
+	if err != nil {
+		log.Fatal("Server shutdown error:", err)
+	}
+	log.Println("Server exiting")
 }
