d5b3cea1de71839dce51a44e7c610f72bed0853b
hulkdx <hulktroll@gmail.com>
2023-05-16 21:26:37 +0300

    Refactoring

--- professional-service/internal/domain/professional/service.go
+++ professional-service/internal/domain/professional/service.go
@@ -1,16 +1,11 @@
 package professional
 
 type Service interface {
-	Repository() Repository
 	FindAllProfessional() ([]Professional, error)
 }
 
 type serviceImpl struct {
 	repository Repository
-}
-
-func (s *serviceImpl) Repository() Repository {
-	return s.repository
 }
 
 func (s *serviceImpl) FindAllProfessional() ([]Professional, error) {
