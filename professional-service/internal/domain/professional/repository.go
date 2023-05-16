47177da6629f24b0bf51ecb207b197bd95541850
hulkdx <hulktroll@gmail.com>
2023-05-16 17:16:49 +0300

    initial impl for repository FindAll

--- professional-service/internal/domain/professional/repository.go
+++ professional-service/internal/domain/professional/repository.go
@@ -17,5 +17,23 @@ type impl struct {
 }
 
 func (p *impl) FindAll() ([]Professional, error) {
-	panic("TODO")
+	query := "SELECT id, email, password, created_at, updated_at FROM professionals"
+	rows, err := p.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	professionals := []Professional{}
+	for rows.Next() {
+		var pro Professional
+		err := rows.Scan(&pro.ID, &pro.Email, &pro.Password, &pro.CreatedAt, &pro.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		professionals = append(professionals, pro)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return professionals, nil
 }
