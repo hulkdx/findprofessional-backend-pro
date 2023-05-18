5780871698650769c11c3a475b305210ed198ba1
hulkdx <hulktroll@gmail.com>
2023-05-18 17:47:23 +0300

    create services

--- professional-service/internal/domain/professional/service.go
+++ professional-service/internal/domain/professional/service.go
@@ -9,6 +9,10 @@ var ErrNotFound = errors.New("not found")
 
 type Service interface {
 	FindAll(context.Context) ([]Professional, error)
+	FindById(ctx context.Context, id string, fields ...string) (Professional, error)
+	Create(ctx context.Context, p Professional) error
+	Delete(ctx context.Context, id string) error
+	Update(ctx context.Context, id string, p Professional) error
 }
 
 type serviceImpl struct {
@@ -21,4 +25,20 @@ func NewService(repository Repository) Service {
 
 func (s *serviceImpl) FindAll(ctx context.Context) ([]Professional, error) {
 	return s.repository.FindAll(ctx, "ID", "Email")
+}
+
+func (s *serviceImpl) FindById(ctx context.Context, id string, fields ...string) (Professional, error) {
+	return s.repository.FindById(ctx, id, fields...)
+}
+
+func (s *serviceImpl) Create(ctx context.Context, p Professional) error {
+	return s.repository.Create(ctx, p)
+}
+
+func (s *serviceImpl) Delete(ctx context.Context, id string) error {
+	return s.repository.Delete(ctx, id)
+}
+
+func (s *serviceImpl) Update(ctx context.Context, id string, p Professional) error {
+	return s.repository.Update(ctx, id, p)
 }
