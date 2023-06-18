56a53813df41c3867df4b488825e1dfeee7d89db
hulkdx <hulktroll@gmail.com>
2023-06-18 15:58:58 +0300

    Fix sql optional issue

--- professional-service/internal/domain/professional/model_professional.go
+++ professional-service/internal/domain/professional/model_professional.go
@@ -6,12 +6,12 @@ type Professional struct {
 	ID              int        `json:"id,omitempty"`
 	Email           string     `json:"email,omitempty"`
 	Password        string     `json:"password,omitempty"`
-	FirstName       string     `json:"first_name,omitempty"`
-	LastName        string     `json:"last_name,omitempty"`
-	CoachType       string     `json:"coach_type,omitempty"`
-	PriceNumber     int        `json:"price_number,omitempty"`
-	PriceCurrency   string     `json:"price_currency,omitempty"`
-	ProfileImageUrl string     `json:"profile_image_url,omitempty"`
+	FirstName       *string     `json:"first_name,omitempty"`
+	LastName        *string     `json:"last_name,omitempty"`
+	CoachType       *string     `json:"coach_type,omitempty"`
+	PriceNumber     *int        `json:"price_number,omitempty"`
+	PriceCurrency   *string     `json:"price_currency,omitempty"`
+	ProfileImageUrl *string     `json:"profile_image_url,omitempty"`
 	CreatedAt       *time.Time `json:"created_at,omitempty"`
 	UpdatedAt       *time.Time `json:"updated_at,omitempty"`
 }
