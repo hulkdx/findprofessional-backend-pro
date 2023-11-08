f578bbf4f416cebafe53184a7b890131a11e6ede
hulkdx <hulktroll@gmail.com>
2023-11-08 18:09:50 +0200

    Availibility filter date

--- professional-service/tests/integration/pro_find_all_integration_test.go
+++ professional-service/tests/integration/pro_find_all_integration_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/domain/professional"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/router"
+	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/utils"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/tests/assert"
 )
 
@@ -77,9 +78,17 @@ func FindAllProfessionalTest(t *testing.T, db *sql.DB) {
 }
 
 func NewTestController(db *sql.DB) *professional.Controller {
+	fakeTime := &FakeTimeProvider{
+		time.Now(),
+	}
+	return NewTestControllerWithTimeProvider(db, fakeTime)
+}
+
+func NewTestControllerWithTimeProvider(db *sql.DB, timeProvider utils.TimeProvider) *professional.Controller {
 	controller := professional.NewController(
-		professional.NewService(professional.NewRepository(db)),
+		professional.NewService(professional.NewRepository(db, timeProvider)),
 		&MockUserService{},
+		timeProvider,
 	)
 	return controller
 }
@@ -88,4 +97,12 @@ type MockUserService struct{}
 
 func (m *MockUserService) IsAuthenticated(ctx context.Context, auth string) bool {
 	return true
+}
+
+type FakeTimeProvider struct {
+	NowTime time.Time
+}
+
+func (p *FakeTimeProvider) Now() time.Time {
+	return p.NowTime
 }
