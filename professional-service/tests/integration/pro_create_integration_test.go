4d9057ea4ba4cbf14bc04d920c88f48449136bc9
hulkdx <hulktroll@gmail.com>
2024-08-17 16:53:32 +0300

    Write the test

--- professional-service/tests/integration/pro_create_integration_test.go
+++ professional-service/tests/integration/pro_create_integration_test.go
@@ -117,6 +117,41 @@ func CreateProTest(t *testing.T, db *sql.DB) {
 		// Asserts
 		assert.Equal(t, response.Code, http.StatusConflict)
 	})
+
+	t.Run("Creating should update user.professionalId", func(t *testing.T) {
+		// Arrange
+		bodyRequest := professional.CreateRequest{
+			Email:         "test@gmail.com",
+			Password:      "P@ssw0rd123",
+			FirstName:     "John",
+			LastName:      "Doe",
+			SkypeId:       "john_doe_skype",
+			AboutMe:       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+			Price:         500,
+			PriceCurrency: "USD",
+			CoachType:     "Lifecoach",
+		}
+		defer db.Exec(`DELETE FROM professionals`)
+
+		user := user.User{
+			ID:        1,
+			Email:     bodyRequest.Email,
+			FirstName: bodyRequest.FirstName,
+			LastName:  bodyRequest.LastName,
+		}
+		d0 := insertUser(t, db, user)
+		defer d0()
+
+		response := httptest.NewRecorder()
+		// Act
+		handler.ServeHTTP(response, createProRequest(bodyRequest))
+		// Asserts
+		assert.Equal(t, response.Code, http.StatusCreated)
+
+		var responseProfessionalId int64 = -1
+		db.QueryRow("SELECT professional_id from users WHERE email=$1", bodyRequest.Email).Scan(&responseProfessionalId)
+		assert.NotEqual(t, responseProfessionalId, -1)
+	})
 }
 
 func createProRequest(body professional.CreateRequest) *http.Request {
