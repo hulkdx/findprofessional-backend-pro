7f03b3926823e2d67de3c203872c5fc6db278ebd
hulkdx <hulktroll@gmail.com>
2025-09-24 16:58:50 +0300

    Fix tests

--- professional-service/tests/mocks/payment.go
+++ professional-service/tests/mocks/payment.go
@@ -8,6 +8,6 @@ import (
 
 type FakePaymentService struct{}
 
-func (s *FakePaymentService) CreatePaymentIntent(ctx context.Context, userId int64, amountInCents int64, currency string, auth string) (*booking_model.PaymentIntentResponse, error) {
+func (s *FakePaymentService) CreatePaymentIntent(ctx context.Context, userId int64, req *booking_model.CreateBookingRequest, auth string) (*booking_model.PaymentIntentResponse, error) {
 	return &booking_model.PaymentIntentResponse{}, nil
 }
