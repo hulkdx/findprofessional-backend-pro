ff7d215828179087b646d48bb3ffa1cec772418a
hulkdx <hulktroll@gmail.com>
2025-11-09 20:14:54 +0200

    Initial success test

--- professional-service/tests/integration/booking_create_integration_test.go
+++ professional-service/tests/integration/booking_create_integration_test.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
-	mocks2 "github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/data/bookingrepo"
+	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/data/bookingrepo"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/domain/booking"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/domain/booking/model"
+	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/domain/payment"
+	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/domain/professional"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/router"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/internal/utils"
 	"github.com/hulkdx/findprofessional-backend-pro/professional-service/tests/assert"
@@ -20,7 +23,43 @@ import (
 func BookingCreateTest(t *testing.T, db *pgxpool.Pool) {
 	timeProvider := &mocks.FakeTimeProvider{}
 	userId := 1
-	handler := router.Handler(NewTestController(db), NewTestBookingController(db, timeProvider, userId))
+	paymentService := mocks.FakePaymentService{}
+	handler := router.Handler(NewTestController(db), NewTestBookingController(db, timeProvider, userId, &paymentService))
+
+	t.Run("success", func(t *testing.T) {
+		// Arrange
+		proId := 1
+		idempotencyKey := "019a4eb2-edb4-7068-b1cb-89bbaee6581f"
+
+		d1 := insertUserWithId(t, db, userId)
+		defer d1()
+
+		d2 := insertPro(t, db, professional.Professional{ID: int64(proId)})
+		defer d2()
+
+		availability := professional.Availability{
+			ProfessionalID: int64(proId),
+			From:           time.Now().Add(time.Hour),
+			To:             time.Now().Add(2 * time.Hour),
+		}
+		availabilityIds, d3 := insertAvailability(t, db, availability)
+		defer d3()
+
+		requestBody := bookingmodel.CreateBookingRequest{
+			Availabilities: []bookingmodel.Availability{{Id: availabilityIds[0]}},
+			IdempotencyKey: idempotencyKey,
+			AmountInCents:  1500,
+			Currency:       "EUR",
+		}
+
+		request := CreateBookingRequest(proId, requestBody)
+		response := httptest.NewRecorder()
+		// Act
+		handler.ServeHTTP(response, request)
+		// Assert
+		assert.Equal(t, response.Code, http.StatusOK)
+		assert.Equal(t, response.Body.String(), http.StatusOK)
+	})
 
 	t.Run("when availability_id doesn't exists return error 404", func(t *testing.T) {
 		// Arrange
@@ -56,9 +95,15 @@ func CreateBookingRequest(proId int, requestBody bookingmodel.CreateBookingReque
 	return request
 }
 
-func NewTestBookingController(db *pgxpool.Pool, timeProvider utils.TimeProvider, userId int) *booking.Controller {
-	repository := mocks2.NewRepository(db, timeProvider)
-	service := booking.NewService(repository, &mocks.FakePaymentService{}, timeProvider)
+func NewTestBookingController(
+	db *pgxpool.Pool,
+	timeProvider utils.TimeProvider,
+	userId int,
+	paymentService payment.Service,
+) *booking.Controller {
 	userService := &MockUserService{UserId: int64(userId)}
+
+	repository := bookingrepo.NewRepository(db, timeProvider)
+	service := booking.NewService(repository, paymentService, timeProvider)
 	return booking.NewController(userService, service)
 }
