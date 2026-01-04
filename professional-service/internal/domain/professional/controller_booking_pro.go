56c9ded6581d46aacf7c952c668a47b665a94783
hulkdx <hulktroll@gmail.com>
2026-01-04 14:42:25 +0200

    Api bookings/user

---
+++ professional-service/internal/domain/professional/controller_booking_pro.go
@@ -0,0 +1,8 @@
+package professional
+
+import (
+	"net/http"
+)
+
+func (c *Controller) GetBookingPro(w http.ResponseWriter, r *http.Request) {
+}
---
+++ professional-service/internal/domain/professional/controller_booking_user.go
@@ -0,0 +1,8 @@
+package professional
+
+import (
+	"net/http"
+)
+
+func (c *Controller) GetBookingUser(w http.ResponseWriter, r *http.Request) {
+}
